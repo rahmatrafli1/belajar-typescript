13cbb231eeef166d4d28c483f338c09b6b168dde
rahmatrafli1 <rahmatrafli1@gmail.com>
2022-08-08 12:37:32 +0700

    change L1-basic-types/index.js

---
+++ dist/L1-basic-types/index.js
@@ -0,0 +1,29 @@
+"use strict";
+// any
+// let obj: any = { a: "some", b: "thing" }
+// obj();
+// Latihan 1
+// let nilaiUjian: unknown;
+// nilaiUjian = 100;
+// if (typeof nilaiUjian === "number") {
+// 	console.log("angka")
+// }
+// if (typeof nilaiUjian === "string") {
+// 	console.log("string")
+// }
+// Latihan 2
+// let a: true = true;
+// a = false;
+// console.log(a);
+// Unknown
+let nilaiUjian;
+nilaiUjian = 100;
+console.log(typeof nilaiUjian);
+let nilaiAkhir;
+if (typeof nilaiUjian === 'number') {
+    nilaiAkhir = nilaiUjian + 100;
+    console.log('nilai akhir: ' + nilaiAkhir);
+}
+else {
+    console.log('tidak diketahui nilai akhirnya');
+}
